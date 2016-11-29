4afbe4c94ad09b55f4a014d54102f0f9ae069461
DarkChocLatte <darkchoclatte@gmail.com>
2016-11-29 16:55:00 +0000

    last commit of day

--- app/app.rb
+++ app/app.rb
@@ -6,6 +6,19 @@ class BookmarksManager < Sinatra::Base
     @links = Link.all
     erb :'links/index'
   end
+
+
+
+  get '/links/new' do
+    erb :new
+
+
+  end
+
+  post '/links/create' do
+
+
+  end
   #
   # # start the server if ruby file executed directly
   run! if app_file == $0
---
+++ spec/feature_test/creating_links_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+feature "should see form" do
+  scenario "When a user visits /links should see" do
+
+      visit '/links/new'
+      fill_in 'title', with: 'This is Bing'
+      fill_in 'url', with: 'https://www.bing.com'
+      click_button 'Bookmark link'
+
+
+      expect(page.current_path).to eq('/links')
+
+      within 'ul#links' do
+        expect(page).to have_content('This is Bing')
+      end
+    end
+  end
