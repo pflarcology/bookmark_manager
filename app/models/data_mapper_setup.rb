f0e72603e003fc2d3883cb0229c58aab850b935f
pflarcology <rhrg500@gmail.com>
2016-12-01 15:02:35 +0000

    removes db autoupgrade

--- app/models/data_mapper_setup.rb
+++ app/models/data_mapper_setup.rb
@@ -7,5 +7,4 @@ require_relative './tag'
 def setup
   DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
   DataMapper.finalize
-  DataMapper.auto_upgrade!
 end
